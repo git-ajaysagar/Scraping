7a56524392e3c633d1f07052c416fc9ebb7b3737
aj.ajay <47740763+git-ajaysagar@users.noreply.github.com>
2020-09-18 21:20:20 +0530

    Create userpass.py

---
+++ userpass.py
@@ -0,0 +1,13 @@
+#This is supposed to be the userpass file which contains username, password of the person applying the instagram bot 
+#and username of the account whose followers list will get downloaded.
+
+# -*- coding: utf-8 -*-
+'''By Ajay'''
+
+username='username'
+
+password='password'   
+
+username_to_search='username_whose_followers_have_to_be_downloaded'
+
+
